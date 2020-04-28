15b1a63d05fa664512c94cac45f408b68a309f5a
arhaiyun <vrhaiyun@gmail.com>
2020-04-28 17:38:30 +0800

    Spring cloud 2020

---
+++ cloud-consumer-order80/src/main/java/com/eiletxie/springcloud/OrderMain80.java
@@ -0,0 +1,21 @@
+package com.eiletxie.springcloud;
+
+import com.eiletxie.myrule.MySelfRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+/**
+ * @Author EiletXie
+ * @Since 2020/3/9 13:49
+ */
+@SpringBootApplication
+@EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
